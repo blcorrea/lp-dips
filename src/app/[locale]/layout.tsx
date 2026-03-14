d30f9c0a148c37d1b497205e45d6066a2bcce9a0
Bruno Correa <blcorrea1@gmail.com>
2026-03-13 22:50:55 -0300

    Remove unused age verification modal import

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,7 +4,6 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 import { routing } from '@/i18n/routing';
 
-import AgeVerificationModal from '@/components/AgeVerificationModal';
 import { CartProvider } from '@/contexts/CartContext';
 import { CustomerProvider } from '@/contexts/CustomerContext';
 
